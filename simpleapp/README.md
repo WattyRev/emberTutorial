efa0e48e2779de5c5241cb8a808133669343b0de
Spencer Watson <spencer@wattydev.com>
2015-09-12 19:45:39 -0700

    switched tutorials again

--- simpleapp/README.md
+++ simpleapp/README.md
@@ -1,4 +1,4 @@
-# Todo-mvc
+# Simpleapp
 
 This README outlines the details of collaborating on this Ember application.
 A short introduction of this app could easily go here.
--- simpleapp/config/environment.js
+++ simpleapp/config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'todo-mvc',
+    modulePrefix: 'simpleapp',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
